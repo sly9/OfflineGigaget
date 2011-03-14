49b5c5e430bce43d47436778ad3eca96a7a92a40
Sun Liuyi <sly@Sun-LiuyimatoMacBook-Pro.local>
2011-03-14 16:00:20 +0800

    finally! logic works!

--- DownloadDelegate.m
+++ DownloadDelegate.m
@@ -20,24 +20,36 @@
     return self;
 }
 
-
+/*
 - (BOOL)respondsToSelector:(SEL)aSelector
 {
     NSString *methodName = NSStringFromSelector(aSelector);
     NSLog(@"DownloadDelegate respondsToSelector: %@", methodName);
     return [super respondsToSelector:aSelector];
-}
+}*/
 
 - (void)downloadDidBegin:(NSURLDownload *)download{
     DownloadJob *job = [[DownloadJob alloc] initWithDownload:download];
     
     if ([self getJobForDownload:download]==nil) {
-        [download setDestination:@"~/temp/" allowOverwrite:NO];
     
         [downloads addObject:job];
         NSLog(@"just added a download job! %@",download);
     }
 }
+
+- (void)download:(NSURLDownload *)download decideDestinationWithSuggestedFilename:(NSString *)filename
+{
+    NSString *destinationFilename;
+    NSString *homeDirectory = NSHomeDirectory();
+    
+    destinationFilename = [[homeDirectory stringByAppendingPathComponent:@"Downloads"]
+                           stringByAppendingPathComponent:filename];
+    
+    [download setDestination:destinationFilename allowOverwrite:NO];
+}
+
+
 
 - (void)downloadDidFinish:(NSURLDownload *)download {
     NSLog(@"%@ download finished!",download);
@@ -46,40 +58,33 @@
     [download release];
     
 }
--(void)download:(NSURLDownload *)download didCreateDestination:(NSString *)path
-{
-    // path now contains the destination path
-    // of the download, taking into account any
-    // unique naming caused by -setDestination:allowOverwrite:
+
+- (void)download:(NSURLDownload *)download didFailWithError:(NSError *)error{
+    NSLog(@"Download failed! Error - %@ %@",
+          [error localizedDescription],
+          [[error userInfo] objectForKey:NSURLErrorFailingURLStringErrorKey]);
+
+}
+
+-(void)download:(NSURLDownload *)download didCreateDestination:(NSString *)path {
     NSLog(@"Final file destination: %@",path);
 }
-- (void)download:(NSURLDownload *)download didReceiveResponse:(NSURLResponse *)response{
+- (void)download:(NSURLDownload *)download didReceiveResponse:(NSURLResponse *)response {
     // received basic info for the download job. create file
     DownloadJob *job = [self getJobForDownload:download];
     job.response = response;
     job.fileSize = [response expectedContentLength];
 }
 
-- (void)download:(NSURLDownload *)download didReceiveDataOfLength:(NSUInteger)length{
+- (void)download:(NSURLDownload *)download didReceiveDataOfLength:(NSUInteger)length {
     DownloadJob *job = [self getJobForDownload:download];
     job.receivedLength += length;
-    NSLog(@"received %ld of %ld",job.receivedLength,job.fileSize);
+//    NSLog(@"received %lld of %lld",job.receivedLength,job.fileSize);
 }
-
 
 
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response{
     //[[NSDistributedNotificationCenter defaultCenter] postNotificationName:@"com.apple.DownloadFileFinished" object:pathToFile];
-	long expectedLength = [response expectedContentLength];
-	long receivedLength = 0;
-	NSLog(@"%@ didReceiveResponse! expected length is:%ld",connection, expectedLength);
-	
-	NSString *tempDir = NSTemporaryDirectory();
-	NSLog(@"%@", tempDir);
-	//NSLog(@"%@/%@", tempDir, fileName);
-	//int ret = [[NSFileManager defaultManager] createFileAtPath:[NSString stringWithFormat:@"%@/%@", tempDir, fileName] contents:nil attributes:nil];
-	//NSLog(@"File creation result: %d", ret);
-	//fileHandle = [[NSFileHandle fileHandleForWritingAtPath:[NSString stringWithFormat:@"%@/%@", tempDir, fileName]] retain];
 }
 
 -(DownloadJob *) getJobForDownload:(NSURLDownload *)download{
